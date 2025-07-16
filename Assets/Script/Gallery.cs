98d3230f25e0bea407a24a00e5b1c3c43cd8e555
win2018hero <star.blade166@gmail.com>
2025-07-16 21:29:23 +0700

    Game UPD V1.3
    Gallery cs Add+ Main Menu

---
+++ Assets/Script/Gallery.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.UIElements;
+
+public class Gallery : MonoBehaviour
+{
+
+    public bool Gallery_Lock;
+    public int Gallery_Index;
+    public  ;
+    public bool clickedAble = false;
+    void Start()
+    {
+        UnlockGallery();
+        Gallery_Index = 0;
+        Gallery_Lock = true;
+        Clickable();
+        StartCoroutine(DelayExample());
+    }
+    void Update()
+    {
+        
+        if (Gallery_Lock == false)
+        {
+            print("False");   
+        }
+    }
+    IEnumerator DelayExample()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+    
+    public void UnlockGallery()
+    {
+        Gallery_Lock = false;
+        Debug.Log("Unlock");
+        clickedAble = Gallery_Lock;
+    }
+    public void LockGallery()
+    {
+        Gallery_Lock = true;
+        Debug.Log("Lock");
+        clickedAble = Gallery_Lock;
+    }
+    public void ToggleGallery()
+    {
+        Gallery_Lock = !Gallery_Lock;
+        Debug.Log("Gallery_Lock = " + Gallery_Lock);
+        UpdateButtonLabel(); 
+    }
+    void UpdateButtonLabel()
+    {
+        Text btnText = toggleButton.GetComponentInChildren<Text>();
+        if (btnText != null)
+        {
+            btnText.text = Gallery_Lock ? "Lock" : "Unlock";
+        }
+    }
+
+    public void Clickable()
+    {
+        if (clickedAble == true)
+        {
+            Debug.Log("1");
+        }
+
+        else if (clickedAble == false)
+        {
+            Debug.Log("0");
+        }
+
+    }
+
+
+}
