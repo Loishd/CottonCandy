3e7cb3f39e68e4bc9f5c2b07da9d853f7f5804d4
win2018hero <chayut.winner@gmail.com>
2025-07-20 00:59:13 +0700

    V1.7.3.2
    Modify little bit

--- Assets/Script/WINBINWON_Edit/HoverEffect.cs
+++ Assets/Script/WINBINWON_Edit/HoverEffect.cs
@@ -7,13 +7,14 @@ public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     private Vector3 hoverPosition;
     private RectTransform rectTransform;
 
-    public float floatAmount = 10f;
-
+    public float floatAmount1 = 10f;
+    public float floatAmount2 = 10f;
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         originalPosition = rectTransform.anchoredPosition;
-        hoverPosition = originalPosition + new Vector3(0, floatAmount, 0);
+        hoverPosition = originalPosition + new Vector3(0, floatAmount1, 0);
+        hoverPosition = originalPosition + new Vector3(floatAmount2,0 , 0);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
